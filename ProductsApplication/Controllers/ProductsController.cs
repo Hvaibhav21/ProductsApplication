6e888556d08860b36c52956d90e3c1f844fadffa
Vaibhav <hvaibhavn@gmail.com>
2023-10-22 21:45:09 +0530

    Added the Create Product Functionality, Scaffolded the Model to create the related view and controller, also created own view for create product and index view as well

--- ProductsApplication/Controllers/ProductsController.cs
+++ ProductsApplication/Controllers/ProductsController.cs
@@ -1,10 +1,18 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NuGet.Protocol;
+using ProductsApplication.Data;
 using ProductsApplication.Models;
 
 namespace ProductsApplication.Controllers
 {
     public class ProductsController : Controller
     {
+        private readonly ProductsApplicationContext _productContext;
+        public ProductsController(ProductsApplicationContext productsApplicationContext) 
+        {
+            _productContext = productsApplicationContext;
+        }
         List<Product> products = new List<Product>
             {
                  new Product(1,"Nivia",3),
@@ -13,12 +21,35 @@ namespace ProductsApplication.Controllers
             };
         
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+
+            var products = await _productContext.Products.ToListAsync();
+
             ViewData["Title"] = "Vaibhav View";
-            return View();
+            return View(products);
         }
 
+
+        [HttpPost]
+        public async Task<IActionResult> AddProduct(Products products)
+        {
+            try
+            {
+                var product = _productContext.Add(products);
+                await _productContext.SaveChangesAsync();
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+
+            return RedirectToAction("Index");
+        }
+        public IActionResult CreateProduct(string name="vaibhav")
+        {
+            return View();
+        }
         public IActionResult Products()
         {
            
@@ -33,23 +64,13 @@ namespace ProductsApplication.Controllers
             ViewData["times"] = times;
             return View();
         }
+
         [HttpPost]
-        public IActionResult Name(NameModel nameData)
+        public IActionResult Name(Products products)
         {
-            try
-            {
-                if (ModelState.IsValid)
-                {
-                    ViewData["Name"] = nameData.Name;
-                    ViewData["times"] = nameData.Times;
-                }
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
-            
-            return View(nameData);
+            var product = _productContext.AddAsync(products);
+            return View(product);
+
         }
 
         public IActionResult setProduct(int id, string name, int validity)
